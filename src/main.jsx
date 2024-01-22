57072f122deac36734f18e2de3c8520fe68f4911
sakibsarkar <sakibsarkar707@gmail.com>
2024-01-22 13:38:25 +0600

    project start

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider />
+  </React.StrictMode>,
+)
