e64efe53d80509def338e62f1bd83356a62b95c2
sakibsarkar <sakibsarkar707@gmail.com>
2024-01-22 14:30:58 +0600

    banner content added

--- src/Pages/Banner/Banner.jsx
+++ src/Pages/Banner/Banner.jsx
@@ -1,9 +1,14 @@
 import "./Banner.css";
+import { Link } from "react-router-dom";
 
 const Banner = () => {
     return (
         <div className="bannerContainer">
-
+            <div className="banner_content">
+                <h1>Find Your Best Dream House for <br /> <span>Rental, Buy & Sell..</span></h1>
+                <p>"House Hunter" is a delightful platform that simplifies the house rental process. It serves as a connecting bridge between house owners, who wish to rent out their properties, and house renters, who are seeking their dream homes</p>
+                <Link>Explore </Link>
+            </div>
         </div>
     );
 };
