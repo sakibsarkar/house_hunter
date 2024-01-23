d1dc51994f8e29c79df709e7fde656f2a45d86fa
sakibsarkar <sakibsarkar707@gmail.com>
2024-01-23 18:16:45 +0600

    login error fixed

--- src/DashBoardPages/Sidebar/Sidebar.jsx
+++ src/DashBoardPages/Sidebar/Sidebar.jsx
@@ -2,13 +2,14 @@ import "./Sidebar.css";
 import OwnerLinks from "../../Components/OwnerLinks";
 import RenterLinks from "../../Components/RenterLinks";
 import { useContext } from "react";
-import { NavLink } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import { Context } from "../../Hooks & Functions/Authcontext";
 
 const Sidebar = () => {
     const { user } = useContext(Context)
     return (
         <div className="sidebarCon">
+            <Link className="logo" to={"/"}>HOUSE <span>HUNTER</span></Link>
             <div className="dashLinks">
                 {
                     user?.role === "House Owner" ?
--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -7,7 +7,6 @@ import { Context } from "../../Hooks & Functions/Authcontext";
 
 const Login = () => {
     const axios = UseAxios()
-    const location = useLocation()
     const navigate = useNavigate()
 
     const { setUser } = useContext(Context)
@@ -19,18 +18,24 @@ const Login = () => {
         const form = e.target
         const email = form.email.value
         const password = form.password.value
+        const toastId = toast.loading("Please wait...")
         const { data } = await axios.post("/login", { email, password })
-        if (data.matched === false) {
+
+        if (data?.matched === false) {
+            toast.dismiss(toastId)
             return toast.error("Password didn't matched")
         }
 
-        if (data.found === false) {
+        if (data?.found === false) {
+            toast.dismiss(toastId)
             return toast.error("Invalid email")
         }
 
         await axios.post("/token", { email, password })
         const address = data?.role === "House Owner" ? "/dashboard/my_rooms" : "/dashboard/my_booking"
         navigate(address)
+        toast.dismiss(toastId)
+        toast.success("Successfuly loged in")
         setUser(data)
     }
     return (
@@ -40,11 +45,11 @@ const Login = () => {
                 <form onSubmit={handleLogin}>
                     <div>
                         <p>Email</p>
-                        <input type="email" name="email" />
+                        <input type="email" name="email" required />
                     </div>
                     <div>
                         <p>password</p>
-                        <input type="password" name="password" />
+                        <input type="password" name="password" required />
                     </div>
                     <button>Login</button>
                 </form>
